f2eb4cbba977c0b7b08d6aa697fe617ad0b7d4a8
sarahibrahim <sisms.1417@gmail.com>
2021-01-27 10:41:41 +0300

    add controller and actor folder

---
+++ src/main/java/com/ga/movieapp/controller/actorController.java
@@ -0,0 +1,32 @@
+package com.ga.movieapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class actorController {
+
+	//CRUD OPERATIONS
+	// C -> Create = Done
+	// R -> Select = Done
+	// U -> Update = Done
+	// D -> Delete = Done
+	
+	
+	@Autowired
+	private Environment env;
+	
+	
+	
+//	//Add actor 
+//	@GetMapping("/actor/add")
+//	public ModelAndView addActor() {
+//		ModelAndView mv = new ModelAndView();
+//		mv.setViewName("actor/add");
+//		
+//	}
+	
+}
