0794ac5f0c88b3dd4b418523ea5ebf25ee91ba05
alaa <alaa.alzahrani88@gmail.com>
2021-01-27 12:10:14 +0300

    add user dao

--- src/main/java/com/ga/movieapp/dao/userDao.java
+++ src/main/java/com/ga/movieapp/dao/userDao.java
@@ -1,5 +1,10 @@
 package com.ga.movieapp.dao;
 
-public class userDao {
+import org.springframework.data.repository.CrudRepository;
 
-}
+import com.ga.movieapp.model.User;
+
+
+public interface UserDao extends CrudRepository<User, Integer> {
+	public User findByEmailAddress(String emailAddress);
+} 
